5df11c62711cfc93d96c96dcb1a4a261d9d58ffe
Jeremy Munene <jeremymunene@gmail.com>
2020-04-20 00:24:51 +0200

    Add more realism to how cards are dropped

---
+++ Assets/Scripts/DiscardPileController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DiscardPileController : MonoBehaviour
+{
+    public List<GameObject> Cards { get; set; } = new List<GameObject>();
+
+    public void DropCard(GameObject card)
+    {
+        var xPosition = gameObject.transform.position.x + Random.Range(-0.03f, 0.03f);
+        var yRotation = gameObject.transform.eulerAngles.y + Random.Range(-35, 23);
+        var zPosition = gameObject.transform.position.z + Random.Range(-0.01f, 0.01f);
+        var cardPosition = new Vector3(xPosition, gameObject.transform.position.y + 0.1f, zPosition);
+
+        var cardGameObject = Instantiate(card, cardPosition, Quaternion.Euler(new Vector3(gameObject.transform.eulerAngles.x, yRotation, 0)));
+        cardGameObject.transform.parent = gameObject.transform;
+    }
+}
--- Assets/Scripts/UsableCardPileController.cs
+++ Assets/Scripts/UsableCardPileController.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class UsableCardPileController : MonoBehaviour
 {
     public Transform discardPile;
-    private List<GameObject> Cards { get; set; }
+    private Stack<GameObject> Cards { get; set; } = new Stack<GameObject>();
 
     //private void Update()
     //{
@@ -23,20 +23,23 @@ public class UsableCardPileController : MonoBehaviour
 
     private void OnMouseDown()
     {
-
+        var currentCard = Cards.Pop();
+        Destroy(currentCard);
+        var discardPileController = discardPile.GetComponent<DiscardPileController>();
+        discardPileController.DropCard(currentCard);
     }
 
     // Instantiate cards, slightly on top of each other
     public void GenerateCards(List<GameObject> cards)
     {
-        Cards = cards;
-
         for (int i = 0; i < cards.Count; i++)
         {
             var card = cards[i];
             var cardGameObject = Instantiate(card, gameObject.transform.position + new Vector3(0f, 0.001f * i, 0f),
-                new Quaternion(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 180f, 0f));
+                Quaternion.Euler(new Vector3(gameObject.transform.eulerAngles.x, gameObject.transform.eulerAngles.y, 180f)));
             cardGameObject.transform.parent = gameObject.transform;
+
+            Cards.Push(cardGameObject);
         }
     }
 }
